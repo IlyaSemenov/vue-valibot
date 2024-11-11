ce50a47ac56a2ee8b9399e5107a62dc14091c54e
Ilya Semenov <ilya@semenov.co>
2024-11-11 11:11:28 +0700

    Update linting to @ilyasemenov/lint-config

--- eslint.config.js
+++ eslint.config.js
@@ -1,25 +1,3 @@
-import config from "@antfu/eslint-config"
+import { defineEslintConfig } from "@ilyasemenov/lint-config"
 
-export default config(
-  {
-    stylistic: {
-      quotes: "double",
-    },
-    rules: {
-      // Always use { } after if/for.
-      "curly": ["error", "all"],
-      "import/order": ["error", {
-        // At least one new line between each group will be enforced, and new lines inside a group will be forbidden.
-        "newlines-between": "always",
-        "alphabetize": {
-          order: "asc",
-          orderImportKind: "asc",
-        },
-      }],
-      "test/consistent-test-it": "off",
-      "no-console": "warn",
-      // One true brace style.
-      "style/brace-style": ["error", "1tbs"],
-    },
-  },
-)
+export default defineEslintConfig()
